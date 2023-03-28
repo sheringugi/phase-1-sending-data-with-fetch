075b350a0d430b2e64d5f8ce30df5cc1ddee1843
sheringugi <sheri.ngugi17@gmail.com>
2023-03-28 11:47:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+  
+function submitData(userName, userEmail ){
+           const formData = {
+            name: userName,
+            email: userEmail
+          }
+          fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData)
+      })
+      .then((resp)=>resp.son())
+      .then((data) => {
+          
+              let p = document.createElement('p')
+              p.textContent = data.id;
+              document.querySelector('body').appendChild(p)
+          })
+      .catch((error)=>{
+          let p= document.createElement('p');
+          p.textContent = error.message;
+          document.querySelector('body').appendChild(p);
+      
+      })
+          
+    
+    
+    
+}
+    
+
